d0969d8cd3d40547b6c7ae6b8ab047be30a2e5b6
Vitor Martins <vitormartins.one@gmail.com>
2022-05-24 23:11:36 -0300

    cadastro de novo usuario

--- alura-angular02/gatitobook/src/app/home/home-routing.module.ts
+++ alura-angular02/gatitobook/src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
@@ -7,10 +8,16 @@ const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    children: [{
-      path: '',
-      component: LoginComponent,
-    }]
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: "novousuario",
+        component: NovoUsuarioComponent,
+      },
+    ]
   }
 ];
 
