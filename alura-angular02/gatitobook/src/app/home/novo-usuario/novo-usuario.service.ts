d29c88dafa6355690860d96c1ae4d07aec38ecad
Vitor Martins <vitormartins.one@gmail.com>
2022-05-24 23:28:17 -0300

    componente e servico de cadastro de novo usuario

---
+++ alura-angular02/gatitobook/src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,15 @@
+import { NovoUsuario } from './novo-usuario';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  cadastraNovoUsuario(novoUsuario: NovoUsuario) {
+    return this.httpClient.post('http://localhost:3000/user/signup', novoUsuario);
+  }
+}
