9a7bf50d2fa679925fdfaaa1032d251b661b93b4
Vitor Martins <vitormartins.one@gmail.com>
2022-05-26 18:33:44 -0300

    autenticacao
     modulo e componente de cabecalho adicionados

--- alura-angular02/gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ alura-angular02/gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,8 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class ServiceNameService {
@@ -13,12 +15,21 @@ export class ServiceNameService {
 })
 export class AutenticacaoService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private usuarioService: UsuarioService) { }
 
-  autenticar(usuario: string, senha: string): Observable<any> {
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      userName: usuario,
-      password: senha,
-    });
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient.post(
+      'http://localhost:3000/user/login',
+      {
+        userName: usuario,
+        password: senha,
+      },
+      { observe: 'response' }
+    ).pipe(
+      tap((response) => {
+        const authToken = response.headers.get('x-access-token') ?? '';
+        this.usuarioService.salvaToken(authToken);
+      })
+    );
   }
 }
---
+++ alura-angular02/gatitobook/src/app/componentes/cabecalho/cabecalho.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CabecalhoComponent } from './cabecalho.component';
+
+
+
+@NgModule({
+  declarations: [CabecalhoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class CabecalhoModule { }
